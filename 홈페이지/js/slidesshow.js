b333986181bc1d6a39799c8c842e1c1a38c9c045
hanjeeung23 <124851946+hanjeeung23@users.noreply.github.com>
2023-03-13 14:19:52 +0900

    Add files via upload

---
+++ 홈페이지/js/slidesshow.js
@@ -0,0 +1,28 @@
+var slide = document.querySelectorAll("#slides > img"); 
+var prev = document.getElementById("prev");
+var next = document.getElementById("next"); 
+
+var current = 0;
+
+showSlide(current); 
+prev.onxlick = prevSlide; 
+next.onclick = nextSlide;
+
+function showSlide(n) { 
+    for (var i = 0;i < slide.length; i++) {
+        slide[i].style.display = "none"; 
+    }
+     slide[n].style.display = "block"; 
+} 
+
+function prevSlide() { 
+    if(current > 0) current -= 1; 
+    else current = slide.length -1; 
+      showSlide(current);
+}
+function nextSlide() { 
+    if(current < slide.length -1) current += 1; 
+
+    else current =0;
+      showSlide(current);
+}
